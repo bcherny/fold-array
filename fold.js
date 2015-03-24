5f01de2dbab9f93341ebcef36af04e55acb74920
Boris Cherny <boris@performancejs.com>
2015-03-24 11:05:38 -0700

    map to days

--- fold.js
+++ fold.js
@@ -27,6 +27,36 @@ assert(fold([1,3,5,6]), '1,3,5,6')
 assert(fold([1,3,5]), '1,3,5')
 
 
+
+function sub (string, map) {
+  return Object
+    .keys(map)
+    .reduce(function (prev, key) {
+      return prev.replace(new RegExp(key, 'g'), map[key])
+    }, string)
+}
+
+const map = {
+  0: 'S',
+  1: 'M',
+  2: 'T',
+  3: 'W',
+  4: 'T',
+  5: 'F',
+  6: 'S'
+}
+
+assert(sub(fold([1,2,3,4,5]), map), 'M-F')
+assert(sub(fold([1,2,3,5]), map), 'M-W,F')
+assert(sub(fold([1,3,5,6]), map), 'M,W,F,S')
+assert(sub(fold([1,3,5]), map), 'M,W,F')
+
+
+
+//////////////////////////
+
+
+
 function assert (a, b) {
   if (a == b) {
     console.info('assertion successful!', a, '==', b)
